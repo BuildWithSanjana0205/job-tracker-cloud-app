0c6e5510acdeacf7eb41ecb17e894d257decdd27
Sanjana Tripathy <tripathysanjana02@gmail.com>
2026-04-08 08:53:37 +0530

    Added backend API with flask and integrated with frontend

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,27 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 
+app = Flask(__name__)
+CORS(app)
+
+jobs = []
+
+@app.route('/jobs', methods=['GET'])
+def get_jobs():
+    return jsonify({"jobs": jobs})
+
+@app.route('/jobs', methods=['POST'])
+def add_job():
+    data = request.get_json()
+
+    job = {
+        "company": data.get("company"),
+        "role": data.get("role")
+    }
+
+    jobs.append(job)
+
+    return jsonify({"message": "Job added", "job": job})
+
+if __name__ == '__main__':
+    app.run(debug=True)
